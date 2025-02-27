1d0654a1c790242578c4cca858f203e309b533cb
MI-LAP-22\candidato <gio.capelli19@gmail.com>
2025-02-27 16:24:46 +0100

    aggiunto metodo sommaPari

--- training-java/src/Test.java
+++ training-java/src/Test.java
@@ -24,4 +24,20 @@ public class Test {
         }
 
     }
+
+    public boolean sommaPari(int x, float y) {
+
+        if(y != Math.floor(y)) {
+            return false;
+        }
+
+        float somma = x+y;
+
+        if(somma%2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
