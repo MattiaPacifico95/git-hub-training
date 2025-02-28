faa2da7c6a7e7c05fa023e542f1b048667cf6822
DESKTOP-AIE0RBU\HP EliteBook 850 G5 <jantrebbi@gmail.com>
2025-02-28 16:36:16 +0100

    Lezione di venerdì 28

---
+++ training-java/src/esercotazioni/car.java
@@ -0,0 +1,42 @@
+public class car implements veicolo, competizione{
+
+    private String color;
+    private int nruote;
+
+    public car (String c) {
+        setColor(c);
+    }
+
+    public void setruote (int numeroRuote) {
+        nruote=numeroRuote;
+    }
+
+
+    public int getRuote (){
+        return nruote;
+
+    }
+
+    public void setColor (String nuovoColore) {
+
+    color = nuovoColore;
+
+    }
+
+    public String getColor () {
+
+        return color;
+    }
+
+    @Override
+    public void clacson() {
+        System.out.println("Beep");
+    }
+
+    public boolean turbo() {
+        return true;
+    }
+}
+
+
+
---
+++ training-java/src/esercotazioni/competizione.java
@@ -0,0 +1,4 @@
+public interface competizione {
+     boolean turbo ();
+
+}
