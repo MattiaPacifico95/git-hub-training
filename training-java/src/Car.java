2a3c48cba845052e03bb8b71fb81d180e1fcd4ad
MI-LAP-22\candidato <gio.capelli19@gmail.com>
2025-02-28 11:30:22 +0100

    creazione classe car

---
+++ training-java/src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    public Car() {
+
+    }
+
+    private String colore;
+
+    public void setColore(String nuovoColore) {
+
+        colore = nuovoColore;
+    }
+
+    public String getColore() {
+        return colore;
+    }
+}
