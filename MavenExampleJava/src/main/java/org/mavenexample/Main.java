b5826348270472623252dab6c3db8340be166d97
MI-LAP-28\candidato <scuolafuturolavorogiannini@gmail.com>
2025-03-03 16:55:36 +0100

    - aggiunti i commenti su MavenExample

--- MavenExampleJava/src/main/java/org/mavenexample/Main.java
+++ MavenExampleJava/src/main/java/org/mavenexample/Main.java
@@ -1,8 +1,9 @@
 package org.mavenexample;
 
-//
+//Main
 public class Main {
     public static void main(String[] args) {
+        //Bottiglia <Tipo generico che viene creato da Vino.java o Acqua.java>
         Bottiglia<Acqua> Levissima = new Bottiglia<Acqua>(new Acqua());
         Bottiglia<Vino> Tavernello = new Bottiglia<Vino>(new Vino());
         Gancio GancioFabbrica = new Gancio();
