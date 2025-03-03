252fd6ef04d9d3a7ce876b30106b2af765487417
MI-LAP-28\candidato <scuolafuturolavorogiannini@gmail.com>
2025-03-03 16:55:36 +0100

    - aggiunti i commenti su MavenExample

--- MavenExampleJava/src/main/java/org/mavenexample/Gancio.java
+++ MavenExampleJava/src/main/java/org/mavenexample/Gancio.java
@@ -2,6 +2,7 @@ package org.mavenexample;
 
 public class Gancio {
     void prendiBottiglia(Bottiglia<?> bottiglia){
+        // get è su Bottiglia.java e dipende dal tipo generico
         System.out.println("Ho presso: " + bottiglia.getContenuto());
     }
 }
