b2490a328989ce5425ce26d53f83a68e058c2e8c
ESTERNO109\Mattia Pacifico <mattia.pacifico@f1cs.it>
2025-03-05 14:31:22 +0100

    add project with structured packages

---
+++ training-java/src/Entities/BaseEntity.java
@@ -0,0 +1,14 @@
+package Entities;
+
+public abstract class BaseEntity {
+
+    long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
