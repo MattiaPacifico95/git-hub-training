b0a24f7070e0b7c90761965d1f00d0e4404db1c2
MI-LAP-28\candidato <scuolafuturolavorogiannini@gmail.com>
2025-03-06 14:57:29 +0100

    - Aggiunto RoleEntity su ictacademy project

---
+++ training-java/src/com/ictacademy/structureproject/entities/RoleEntity.java
@@ -0,0 +1,44 @@
+package com.ictacademy.structureproject.entities;
+
+import java.util.Objects;
+
+public class RoleEntity {
+
+    // Role Proprieta
+    long idRole;
+    String name;
+
+    // Costruttore
+    public RoleEntity() {
+    }
+
+    // Role Get e Set
+    public long getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(long idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Equals e Hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleEntity that = (RoleEntity) o;
+        return idRole == that.idRole && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idRole, name);
+    }
+}
