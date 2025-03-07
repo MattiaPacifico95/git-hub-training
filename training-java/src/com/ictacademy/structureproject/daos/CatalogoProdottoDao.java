c283ab959d8a305341849d1ad02e31d6701782b5
MI-LAP-22\candidato <gio.capelli19@gmail.com>
2025-03-07 10:23:56 +0100

    creazione catalogoprodottodao

---
+++ training-java/src/com/ictacademy/structureproject/daos/CatalogoProdottoDao.java
@@ -0,0 +1,91 @@
+package com.ictacademy.structureproject.daos;
+
+import com.ictacademy.structureproject.entities.CatalogoProdottoEntity;
+import com.ictacademy.structureproject.utils.DbConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoProdottoDao {
+
+    DbConnection dbConnection = new DbConnection();
+
+    public List<CatalogoProdottoEntity> findAll() throws SQLException {
+
+        List<CatalogoProdottoEntity> listaCatalogoProdotto = new ArrayList<CatalogoProdottoEntity>();
+
+        Connection conn = dbConnection.creaConnessione();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from CatalogoProdotto");
+
+        while(rs.next()){
+            CatalogoProdottoEntity catalogoProdotto = new CatalogoProdottoEntity();
+            catalogoProdotto.setIdCatalogo(rs.getLong("id_catalogo"));
+            catalogoProdotto.setIdProdotto(rs.getLong("id_prodotto"));
+            listaCatalogoProdotto.add(catalogoProdotto);
+        }
+        conn.close();
+
+        return listaCatalogoProdotto;
+    }
+
+    public List<CatalogoProdottoEntity> findByIdProdotto(long idProdotto) throws SQLException {
+
+        List<CatalogoProdottoEntity> listaCatalogoProdotto = new ArrayList<CatalogoProdottoEntity>();
+
+        Connection conn = dbConnection.creaConnessione();
+        PreparedStatement ps = conn.prepareStatement("select * from CatalogoProdotto where id_prodotto = ?");
+        ps.setLong(1, idProdotto);
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()){
+            CatalogoProdottoEntity catalogoProdotto = new CatalogoProdottoEntity();
+            catalogoProdotto.setIdCatalogo(rs.getLong("id_catalogo"));
+            catalogoProdotto.setIdProdotto(rs.getLong("id_prodotto"));
+            listaCatalogoProdotto.add(catalogoProdotto);
+        }
+        conn.close();
+
+        return listaCatalogoProdotto;
+    }
+
+    public List<CatalogoProdottoEntity> findByIdCatalogo(long idCatalogo) throws SQLException {
+
+        List<CatalogoProdottoEntity> listaCatalogoProdotto = new ArrayList<CatalogoProdottoEntity>();
+
+        Connection conn = dbConnection.creaConnessione();
+        PreparedStatement ps = conn.prepareStatement("select * from CatalogoProdotto where id_catalogo = ?");
+        ps.setLong(1, idCatalogo);
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()){
+            CatalogoProdottoEntity catalogoProdotto = new CatalogoProdottoEntity();
+            catalogoProdotto.setIdCatalogo(rs.getLong("id_catalogo"));
+            catalogoProdotto.setIdProdotto(rs.getLong("id_prodotto"));
+            listaCatalogoProdotto.add(catalogoProdotto);
+        }
+        conn.close();
+
+        return listaCatalogoProdotto;
+    }
+
+    public boolean createCatalogoProdotto(long idCatalogo, long idProdotto) throws SQLException {
+        boolean created;
+        Connection conn = dbConnection.creaConnessione();
+        PreparedStatement ps = conn.prepareStatement("insert into CatalogoProdotto (id_catalogo, id_prodotto) values (?, ?)");
+        ps.setLong(1, idCatalogo);
+        ps.setLong(2, idProdotto);
+        int rs = ps.executeUpdate();
+        conn.close();
+
+        if(rs>0) {
+            created = true;
+        } else {
+            created = false;
+        }
+        return created;
+    }
+
+
+}
