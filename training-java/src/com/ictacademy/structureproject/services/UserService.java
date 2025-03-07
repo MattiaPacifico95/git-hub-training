085a580eca11a680a891eead81ac33ca058a6f93
ESTERNO109\Mattia Pacifico <mattia.pacifico@f1cs.it>
2025-03-07 16:59:38 +0100

    add user service and login method

--- training-java/src/com/ictacademy/structureproject/services/UserService.java
+++ training-java/src/com/ictacademy/structureproject/services/UserService.java
@@ -1,5 +1,6 @@
 package com.ictacademy.structureproject.services;
 
+import com.ictacademy.structureproject.daos.CredentialManagerDao;
 import com.ictacademy.structureproject.daos.UserDao;
 import com.ictacademy.structureproject.entities.UserEntity;
 
@@ -8,7 +9,7 @@ import java.sql.SQLException;
 public class UserService {
 
     UserDao userDao = new UserDao();
-    CredentialManagerDao = new CredentialManager();
+    CredentialManagerDao credentialManagerDao = new CredentialManagerDao();
 
     /*
     LOGIN: funzione che prende in ingresso email
@@ -17,9 +18,23 @@ public class UserService {
      */
     public UserEntity login(String email, String password) throws SQLException {
 
-        UserEntity utente = new UserEntity();
-        utente = userDao.findByEmail(email);
-        return utente;
+
+        UserEntity utente = userDao.findByEmail(email);
+
+        if(utente != null) {
+            System.out.println("esiste l'utente con mail"+ email);
+            long idUtente = utente.getIdUtente();
+            String psw = credentialManagerDao.findByPswAndIdUtente(password, idUtente);
+            if(psw != null) {
+                System.out.println("la password coincide");
+                return utente;
+            } else {
+                System.out.println("la password non coincide");
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 
 
