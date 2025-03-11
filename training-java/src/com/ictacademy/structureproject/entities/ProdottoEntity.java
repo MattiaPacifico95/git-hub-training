6d6f20eef3a9ff77e96feed5e06fbdb44e32f24b
MI-LAP-25\candidato <dalessandrodavide10@gmail.com>
2025-03-11 16:44:40 +0100

    Modificato ProdottoDao e ProdottoEntity, Creato ProdottoService, Ricreato DBConnection dopo errore

--- training-java/src/com/ictacademy/structureproject/entities/ProdottoEntity.java
+++ training-java/src/com/ictacademy/structureproject/entities/ProdottoEntity.java
@@ -3,11 +3,11 @@ package com.ictacademy.structureproject.entities;
 import java.sql.Timestamp;
 import java.util.Objects;
 
-
 public class ProdottoEntity {
     long id_prodotto;
     String nome;
     String descrizione;
+    int punti;
     Timestamp data_creazione;
     Boolean flg_cancellato;
 
@@ -15,6 +15,13 @@ public class ProdottoEntity {
     public ProdottoEntity() {
     }
 
+    public int getPunti() {
+        return punti;
+    }
+
+    public void setPunti(int punti) {
+        this.punti = punti;
+    }
 
     public long getId_prodotto() {
         return id_prodotto;
@@ -60,12 +67,12 @@ public class ProdottoEntity {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         ProdottoEntity that = (ProdottoEntity) o;
-        return id_prodotto == that.id_prodotto && Objects.equals(nome, that.nome) && Objects.equals(descrizione, that.descrizione) && Objects.equals(data_creazione, that.data_creazione) && Objects.equals(flg_cancellato, that.flg_cancellato);
+        return id_prodotto == that.id_prodotto && punti == that.punti && Objects.equals(nome, that.nome) && Objects.equals(descrizione, that.descrizione) && Objects.equals(data_creazione, that.data_creazione) && Objects.equals(flg_cancellato, that.flg_cancellato);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id_prodotto, nome, descrizione, data_creazione, flg_cancellato);
+        return Objects.hash(id_prodotto, nome, descrizione, punti, data_creazione, flg_cancellato);
     }
 
     @Override
@@ -74,6 +81,7 @@ public class ProdottoEntity {
                 "id_prodotto=" + id_prodotto +
                 ", nome='" + nome + '\'' +
                 ", descrizione='" + descrizione + '\'' +
+                ", punti=" + punti +
                 ", data_creazione=" + data_creazione +
                 ", flg_cancellato=" + flg_cancellato +
                 '}';
---
+++ training-java/src/com/ictacademy/structureproject/services/ProdottoService.java
@@ -0,0 +1,53 @@
+package com.ictacademy.structureproject.services;
+import com.ictacademy.structureproject.daos.*;
+import com.ictacademy.structureproject.entities.ProdottoEntity;
+import com.ictacademy.structureproject.entities.UserEntity;
+import com.ictacademy.structureproject.utils.DbConnection;
+
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdottoService {
+    ProdottoDao prodottoDao = new ProdottoDao();
+    CatalogoDao catalogoDao = new CatalogoDao();
+    CatalogoProdottoDao catalogoProdottoDao = new CatalogoProdottoDao();
+
+    List<ProdottoEntity> listaProdotti = new ArrayList<>();
+
+    public List<ProdottoEntity> aggiungiProdotto(long idProdotto) throws SQLException {
+
+
+        ProdottoEntity prodottoEsistente = prodottoDao.findById(idProdotto);
+
+        if (prodottoEsistente != null && prodottoEsistente.getId_prodotto() > 0) {
+            listaProdotti.add(prodottoEsistente);
+        } else {
+            System.out.println("Prodotto con ID " + idProdotto + " non trovato.");
+        }
+
+        return listaProdotti;
+    }
+
+
+    public List<ProdottoEntity> cancellaProdotto(long idProdotto) throws SQLException {
+
+        ProdottoEntity prodottoDaCancellare = null;
+        for (ProdottoEntity prodotto : listaProdotti) {
+            if (prodotto.getId_prodotto() == idProdotto) {
+                prodottoDaCancellare = prodotto;
+                break;
+            }
+        }
+
+        if (prodottoDaCancellare != null) {
+            listaProdotti.remove(prodottoDaCancellare);
+        } else {
+            System.out.println("Prodotto con ID " + idProdotto + " non trovato nella lista.");
+        }
+
+        return listaProdotti;
+    }
+}
