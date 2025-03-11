609d3c5b8ce871d720ac232555d8c3e4085f19a1
MI-LAP-28\candidato <scuolafuturolavorogiannini@gmail.com>
2025-03-11 12:26:37 +0100

    aggiunta registrazione utenti in UserService

--- training-java/src/com/ictacademy/structureproject/services/UserService.java
+++ training-java/src/com/ictacademy/structureproject/services/UserService.java
@@ -3,7 +3,10 @@ package com.ictacademy.structureproject.services;
 import com.ictacademy.structureproject.daos.CredentialManagerDao;
 import com.ictacademy.structureproject.daos.UserDao;
 import com.ictacademy.structureproject.entities.UserEntity;
+import com.ictacademy.structureproject.utils.DbConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UserService {
@@ -36,6 +39,31 @@ public class UserService {
             return null;
         }
     }
+    // Registrazione
+    public boolean Registration(String emailToFind, UserEntity newUser) throws SQLException {
+        // Crea connessione
+        DbConnection dbConnection = new DbConnection();
+        UserEntity utente = userDao.findByEmail(emailToFind);
+        boolean AccountCreate;
+        if (utente != null){
+            System.out.println("L'utente esiste gia.\n");
+            return AccountCreate = false;
+        }
 
+        // prende CreateUser in dao
+        AccountCreate = userDao.createUser(newUser.getNome(),newUser.getCognome(),newUser.getEmail(),newUser.getDataNascita());
+        return AccountCreate;
 
+//        Connection conn = dbConnection.creaConnessione();
+//        PreparedStatement ps = conn.prepareStatement("insert into User (idUtente ,id_role ,nome ,cognome ,email ,dataNascita ,flagCancellato) VALUES (?, ?, ?, ?; ?; ?, ?");
+//        ps.setLong(1, newUser.getIdUtente());
+//        ps.setLong(2, newUser.getIdRuolo());
+//        ps.setString(3, newUser.getNome());
+//        ps.setString(4, newUser.getCognome());
+//        ps.setString(5, newUser.getEmail());
+//        ps.setTimestamp(6, newUser.getDataNascita());
+//        ps.setBoolean(7, newUser.isFlagCancellato());
+//        int rs = ps.executeUpdate();
+//        conn.close();
+    }
 }
