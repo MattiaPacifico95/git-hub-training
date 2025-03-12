4981f67195328188a0429849d80bb857a7ddf7e2
DESKTOP-AIE0RBU\HP EliteBook 850 G5 <jantrebbi@gmail.com>
2025-03-12 11:05:10 +0100

    Commit CatalogoService

--- training-java/src/com/ictacademy/structureproject/services/CatalogoService.java
+++ training-java/src/com/ictacademy/structureproject/services/CatalogoService.java
@@ -4,9 +4,11 @@ package com.ictacademy.structureproject.services;
 import com.ictacademy.structureproject.daos.CatalogoDao;
 import com.ictacademy.structureproject.daos.ProdottoDao;
 import com.ictacademy.structureproject.daos.CatalogoProdottoDao;
+import com.ictacademy.structureproject.daos.UserDao;
 import com.ictacademy.structureproject.entities.CatalogoEntity;
 import com.ictacademy.structureproject.entities.ProdottoEntity;
 import com.ictacademy.structureproject.entities.CatalogoProdottoEntity;
+import com.ictacademy.structureproject.entities.UserEntity;
 
 public class CatalogoService {
 
@@ -14,13 +16,27 @@ public class CatalogoService {
     ProdottoDao prodottoDao = new ProdottoDao();
     CatalogoProdottoDao catalogoProdottoDao = new CatalogoProdottoDao();
     CatalogoProdottoService catalogoProdottoService = new CatalogoProdottoService();
+    UserEntity userEntity = new UserEntity();
 
-
+    //lista prodotti con prezzo e descrizione... penso?
     public List<ProdottoEntity> elencaProdotti(long idCatalogo) {
 
         return catalogoProdottoService.getProdottiForCatalogo(idCatalogo);
 
     }
+
+    //questo dovrebbe far aggiornare il catalogo per l'admin
+    public boolean gestisciCatalogo (){
+        boolean catAggiornato;
+        if (userEntity.getIdRuolo() != 1) {
+            return catAggiornato = false;
+        }
+
+        
+
+    }
+
+
 
     
 }
