7e99e6c15f9d72dc56a1613dfb96d7b697a10182
MI-LAP-22\candidato <gio.capelli19@gmail.com>
2025-03-17 16:53:39 +0100

    correzione errore catproddao

--- spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/daos/CatalogoProdottoDao.java
+++ spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/daos/CatalogoProdottoDao.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository // metodi CRUD già forniti da SPRING
-public interface CataogoProdottoDao extends JpaRepository<CatalogoProdottoEntity, Long> {
+public interface CatalogoProdottoDao extends JpaRepository<CatalogoProdottoEntity, Long> {
 
 }
