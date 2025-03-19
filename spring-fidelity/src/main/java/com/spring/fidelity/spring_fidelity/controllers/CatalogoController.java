a83451d51551bd9de4958075d67a52c2f95eecaf
DESKTOP-AIE0RBU\HP EliteBook 850 G5 <jantrebbi@gmail.com>
2025-03-19 15:49:22 +0100

    Altra modifica

--- spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/controllers/CatalogoController.java
+++ spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/controllers/CatalogoController.java
@@ -1,4 +1,14 @@
 package com.spring.fidelity.spring_fidelity.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CatalogoController {
+    @GetMapping("/Catalogo")
+    public String catalogo(){
+        return "Prova";
+    }
 }
+
