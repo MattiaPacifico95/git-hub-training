e31193f36bde8dc4016e37c921e6d994a00d33e1
MI-LAP-25\candidato <dalessandrodavide10@gmail.com>
2025-03-19 11:09:03 +0100

    Creazione ProdottoService

--- spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/daos/ProdottoDao.java
+++ spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/daos/ProdottoDao.java
@@ -3,7 +3,16 @@ package com.spring.fidelity.spring_fidelity.daos;
 import com.spring.fidelity.spring_fidelity.entities.ProdottoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.NativeQuery;
+
+import java.sql.Timestamp;
+
 
 @Repository
 public interface ProdottoDao extends JpaRepository<ProdottoEntity, Long> {
+    @NativeQuery("select * from prodotto where id_prodotto = ?")
+    public ProdottoEntity findById(long idProdotto);
+
+    @NativeQuery("UPDATE into Prodotto (nome, descrizione, punti, dataCreazione) VALUES (?,?,?,?)")
+    public ProdottoEntity updateProdotto(String nome, String descrizione, int punti, Timestamp dataCrerazione);
 }
