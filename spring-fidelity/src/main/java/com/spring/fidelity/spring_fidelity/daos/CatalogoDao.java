0c42aea60c98a0ef5b8b3c6d048e35439581136c
MI-LAP-22\candidato <gio.capelli19@gmail.com>
2025-03-20 12:22:09 +0100

    modifiche catdao e catservice

--- spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/daos/CatalogoDao.java
+++ spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/daos/CatalogoDao.java
@@ -1,13 +1,19 @@
 package com.spring.fidelity.spring_fidelity.daos;
 
 import com.spring.fidelity.spring_fidelity.entities.CatalogoEntity;
+import com.spring.fidelity.spring_fidelity.entities.ProdottoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CatalogoDao extends JpaRepository<CatalogoEntity,Long> {
     @NativeQuery("select * from catalogo where id_catalogo = ?")
     public CatalogoEntity findByID(long id_utente);
+
+    @NativeQuery("SELECT id_prodotto FROM catalogo_prodotto WHERE id_catalogo = ?1")
+    public List<Long> listProdInCat(long idCatalogo);
 }
--- spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/services/CatalogoService.java
+++ spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/services/CatalogoService.java
@@ -7,19 +7,26 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.spring.fidelity.spring_fidelity.daos.CatalogoDao;
+import com.spring.fidelity.spring_fidelity.daos.ProdottoDao;
 import com.spring.fidelity.spring_fidelity.entities.CatalogoEntity;
+import com.spring.fidelity.spring_fidelity.entities.ProdottoEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CatalogoService {
+    private final ProdottoDao prodottoDao;
     private CatalogoDao catDao;
     private CatalogoEntity catalogo;
 
-    public CatalogoService (CatalogoDao catDao) {
+    public CatalogoService (CatalogoDao catDao, ProdottoDao prodottoDao) {
         this.catDao = catDao;
+        this.prodottoDao = prodottoDao;
     }
 
 
@@ -40,6 +47,16 @@ public class CatalogoService {
     public void emergencyFlag (CatalogoEntity catalogo) {
         this.catalogo.setFlagCancellato(true);
 
+    }
+
+    public List<ProdottoEntity> findProdottiInCatalogo (Long idCatalogo) {
+        List<ProdottoEntity> listaProdotti = new ArrayList<ProdottoEntity>();
+        List<Long> lIdProd = catDao.listProdInCat(idCatalogo);
+        for(Long idProdotto : lIdProd){
+            ProdottoEntity prod = prodottoDao.findByID(idCatalogo);
+            listaProdotti.add(prod);
+        }
+        return listaProdotti;
     }
 
 
