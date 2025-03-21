56b1770a94e10c5adf9b851dacee4554361d32de
DESKTOP-AIE0RBU\HP EliteBook 850 G5 <jantrebbi@gmail.com>
2025-03-21 14:40:48 +0100

    no message

--- spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/dtos/CatalogoDto.java
+++ spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/dtos/CatalogoDto.java
@@ -1,4 +1,74 @@
 package com.spring.fidelity.spring_fidelity.dtos;
+import lombok.Data;
 
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Data
 public class CatalogoDto {
+
+    private long catID;
+
+    private String trueName;
+
+    private Timestamp endOffer;
+
+    private boolean flagCancelled;
+
+
+    public long getCatID() {
+        return catID;
+    }
+
+    public void setCatID(long catID) {
+        this.catID = catID;
+    }
+
+    public String getTrueName() {
+        return trueName;
+    }
+
+    public void setTrueName(String trueName) {
+        this.trueName = trueName;
+    }
+
+    public Timestamp getEndOffer() {
+        return endOffer;
+    }
+
+    public void setEndOffer(Timestamp endOffer) {
+        this.endOffer = endOffer;
+    }
+
+    public boolean isFlagCancelled() {
+        return flagCancelled;
+    }
+
+    public void setFlagCancelled(boolean flagCancelled) {
+        this.flagCancelled = flagCancelled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CatalogoDto that = (CatalogoDto) o;
+        return catID == that.catID && flagCancelled == that.flagCancelled && Objects.equals(trueName, that.trueName) && Objects.equals(endOffer, that.endOffer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(catID, trueName, endOffer, flagCancelled);
+    }
+
+    @Override
+    public String toString() {
+        return "CatalogoDto{" +
+                "catID=" + catID +
+                ", trueName='" + trueName + '\'' +
+                ", endOffer=" + endOffer +
+                ", flagCancelled=" + flagCancelled +
+                '}';
+    }
 }
+
+
--- spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/utils/CatalogoConverter.java
+++ spring-fidelity/src/main/java/com/spring/fidelity/spring_fidelity/utils/CatalogoConverter.java
@@ -1,4 +1,35 @@
 package com.spring.fidelity.spring_fidelity.utils;
 
+import com.spring.fidelity.spring_fidelity.dtos.CatalogoDto;
+import com.spring.fidelity.spring_fidelity.entities.CatalogoEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CatalogoConverter {
+
+
+    public CatalogoDto toDto (CatalogoEntity catalogoEntity){
+
+        CatalogoDto catDto = new CatalogoDto();
+
+        catDto.setCatID(catalogoEntity.getIdCatalogo());
+        catDto.setTrueName(catalogoEntity.getNome());
+        catDto.setEndOffer(catalogoEntity.getFineOfferta());
+        catDto.setFlagCancelled(catalogoEntity.isFlagCancellato());
+
+        return catDto;
+    }
+
+    public CatalogoEntity toEntity (CatalogoDto catalogoDto){
+
+        CatalogoEntity catEntity = new CatalogoEntity();
+
+
+        catEntity.setIdCatalogo(catalogoDto.getCatID());
+        catEntity.setNome(catalogoDto.getTrueName());
+        catEntity.setFineOfferta(catalogoDto.getEndOffer());
+        catEntity.setFlagCancellato(catalogoDto.isFlagCancelled());
+
+        return catEntity;
+    }
+
 }
